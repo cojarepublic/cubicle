d7ff947cc5a30b8e8832160da80d00a9ebd7c8ab
cojarepublic <60820059+cojarepublic@users.noreply.github.com>
2020-08-13 09:58:42 +0100

    Ataching orders to users

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
             builder: (ctx) => Cart(),
           ),
           ChangeNotifierProxyProvider<Auth, Orders>(
-            builder: (ctx, auth, previousOrders) => Orders(auth.token, previousOrders == null ? [] : previousOrders.orders),
+            builder: (ctx, auth, previousOrders) => Orders(auth.token, auth.userId, previousOrders == null ? [] : previousOrders.orders),
           ),
         ],
         child: Consumer<Auth>(
