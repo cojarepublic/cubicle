645a2c896daf6e2f329f45ea1d0ea51acc1f3799
cojarepublic <60820059+cojarepublic@users.noreply.github.com>
2020-08-13 10:47:53 +0100

    automatically logging users out after some time feature

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:cubicle/models/http_exception.dart';
 import 'package:flutter/widgets.dart';
@@ -9,6 +10,7 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -46,6 +48,7 @@ class Auth with ChangeNotifier {
     _token = responseData['idToken'];
     _userId = responseData['localId'];
     _expiryDate = DateTime.now().add(Duration(seconds: int.parse(responseData['expiresIn']),));
+    _autoLogout();
     notifyListeners();
     } catch (error) {
       throw error;
@@ -64,7 +67,20 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
 
   }
+
+  void _autoLogout () {
+    if (_authTimer != null) {
+      _authTimer.cancel();
+    }
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
+  }
+
 }
