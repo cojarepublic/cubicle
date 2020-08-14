4d27481ff677cc83a8aa2466eff36293f137ff08
cojarepublic <60820059+cojarepublic@users.noreply.github.com>
2020-08-14 08:26:20 +0100

    using the animated builder widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -118,9 +118,9 @@ class _AuthCardState extends State<AuthCard>
             begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
         .animate(
             CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn));
-    _heightAnimation.addListener(() {
-      setState(() {});
-    });
+//    _heightAnimation.addListener(() {
+//      setState(() {});
+//    });
   }
 
   @override
@@ -218,13 +218,13 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
+      child: AnimatedBuilder(animation: _heightAnimation, builder: (ctx, ch) => Container(
 //        height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
-        child: Form(
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: ch),  child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
